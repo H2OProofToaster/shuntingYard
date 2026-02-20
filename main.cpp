36584e6f0838ae5b29445cf885c1d3aa4eda3120
Nick <188719678+H2OProofToaster@users.noreply.github.com>
2026-02-20 09:46:33 -0800

    structure done

--- main.cpp
+++ main.cpp
@@ -1,8 +1,50 @@
 #include <iostream>
+#include <string>
+
+#include "structs.cpp"
+
+string shuntingYard(string input);
+Node* makeExpressionTree(string input);
+
+void printInfix(Node* input);
+void printPostfix(Node* input);
+void printPrefix(Node* input);
 
 int main() {
 
+  //Ask for input
+  string input;
+  cout << "What is the input (infix)? ";
+  cin >> input;
+
+  //Do shunting yard algorithm
+  cout << "Shunting..." << endl;
+
+  string postfix = shuntingYard(input);
+  cout << "Shunting yard postfix: " << postfix << endl;
   
-  
+  cout << "Done" << endl;
+
+  //Make bianry expression tree
+  cout << "Making tree..." << endl;
+
+  Node* tree = makeExpressionTree(postfix);
+
+  cout << "Done" << endl;
+
+  //Ask for output
+  string outputType;
+  while (outputType.empty()) {
+    cout << "INFIX, POSTFIX, or PREFIX? ";
+    cin >> outputType;
+
+    if (outputType == "INFIX" or outputType == "i") { printInfix(tree); }
+    else if (outputType == "POSTFIX" or outputType == "post") { printPostfix(tree); }
+    else if (outputType == "PREFIX" or outputType == "pre") { printPrefix(tree); }
+    else { cout << "Not an option" << endl; }
+
+    outputType.clear();
+  }
+    
   return 0;
 }
